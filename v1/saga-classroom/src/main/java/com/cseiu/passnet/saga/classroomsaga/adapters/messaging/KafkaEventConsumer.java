078fe8c5a707f8c1c3f743073d65bda6de6f839b
DuongVu98 <duongvutung1998@gmail.com>
2021-03-15 02:17:30 +0700

    done: stuff

--- v1/saga-classroom/src/main/java/com/cseiu/passnet/saga/classroomsaga/adapters/messaging/KafkaEventConsumer.java
+++ v1/saga-classroom/src/main/java/com/cseiu/passnet/saga/classroomsaga/adapters/messaging/KafkaEventConsumer.java
@@ -1,26 +1,20 @@
 package com.cseiu.passnet.saga.classroomsaga.adapters.messaging;
 
-import com.cse.iu.passnet.saga.avro.AcceptStudentApplicationEventAvro;
-import com.cse.iu.passnet.saga.avro.DeleteJobEventAvro;
-import com.cse.iu.passnet.saga.avro.PostNewJobEventAvro;
-import com.cse.iu.passnet.saga.avro.RemoveStudentApplicationEventAvro;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.specific.SpecificRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaHandler;
+import com.cse.iu.passnet.saga.avro.*;
+
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 @Component
 @Slf4j(topic = "[KafkaEventConsumer]")
 public class KafkaEventConsumer {
 
-    @KafkaListener(topics = "post-new-job-event", containerFactory = "postNewJobEventContainerFactory")
+    @KafkaListener(topics = "post-new-job-event")
     public void listenPostNewJobEvent(@Payload PostNewJobEventAvro event){
-        log.info("log from Consumer");
         log.info("log record --> {}", event);
     }
 
