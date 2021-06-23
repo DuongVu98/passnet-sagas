76078014b4bc78be7858ad0d26f393f2f777ee03
DuongVu98 <duongvutung1998@gmail.com>
2021-06-24 02:30:32 +0700

    add service name

--- v1/saga-user/src/main/java/com/cseiu/passnet/saga/usersaga/common/models/DomainServiceNames.java
+++ v1/saga-user/src/main/java/com/cseiu/passnet/saga/usersaga/common/models/DomainServiceNames.java
@@ -2,6 +2,7 @@ package com.cseiu.passnet.saga.usersaga.common.models;
 
 public enum DomainServiceNames {
     RECRUITMENT_SERVICE,
-    AUTHENTICATION_SERVICE,
-    CLASSROOM_SERVICE
+    USER_SERVICE,
+    CLASSROOM_SERVICE,
+    PROFILE_SERVICE
 }
