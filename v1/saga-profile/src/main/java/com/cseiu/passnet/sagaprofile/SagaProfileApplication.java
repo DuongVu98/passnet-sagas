3542bd9a0925092b2dd505aa3ddee21765278787
DuongVu98 <duongvutung1998@gmail.com>
2021-06-24 01:02:04 +0700

    add saga-profile

---
+++ v1/saga-profile/src/main/java/com/cseiu/passnet/sagaprofile/SagaProfileApplication.java
@@ -0,0 +1,13 @@
+package com.cseiu.passnet.sagaprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaProfileApplication.class, args);
+    }
+
+}
---
+++ v1/saga-profile/src/test/java/com/cseiu/passnet/sagaprofile/SagaProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cseiu.passnet.sagaprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
