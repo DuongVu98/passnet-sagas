e5ef746e992e73c861edf32e4b0e8725bbc03e34
DuongVu98 <duongvutung1998@gmail.com>
2021-07-03 02:24:11 +0700

    test grpc

--- v1/saga-profile/src/main/java/com/cseiu/passnet/sagaprofile/flow/consume/api/ResponseListener.java
+++ v1/saga-profile/src/main/java/com/cseiu/passnet/sagaprofile/flow/consume/api/ResponseListener.java
@@ -2,13 +2,18 @@ package com.cseiu.passnet.sagaprofile.flow.consume.api;
 
 import com.cse.iu.passnet.saga.avro.FailureResponse;
 import com.cse.iu.passnet.saga.avro.SuccessResponse;
+import com.cseiu.passnet.sagaprofile.flow.consume.core.executor.ConsumingExecutor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j(topic = "[ResponseListener]")
 public class ResponseListener {
+
+    @Autowired
+    ConsumingExecutor consumingExecutor;
 
     @KafkaListener(topics = "${config.messaging.topics.success-response}")
     public void listen(SuccessResponse response) {
@@ -18,5 +23,6 @@ public class ResponseListener {
     @KafkaListener(topics = "${config.messaging.topics.failure-response}")
     public void listen(FailureResponse response){
         log.info("about to consume failure response message for event: [{}]", response.getEventId());
+        consumingExecutor.consume(response);
     }
 }
--- v1/saga-profile/src/main/java/com/cseiu/passnet/sagaprofile/flow/consume/core/consumer/FailureResponseConsumer.java
+++ v1/saga-profile/src/main/java/com/cseiu/passnet/sagaprofile/flow/consume/core/consumer/FailureResponseConsumer.java
@@ -1,12 +1,43 @@
 package com.cseiu.passnet.sagaprofile.flow.consume.core.consumer;
 
 import com.cse.iu.passnet.saga.avro.FailureResponse;
+import com.cseiu.passnet.sagaprofile.common.exceptions.EventNotFoundException;
+import com.cseiu.passnet.sagaprofile.common.exceptions.SagaNotFoundException;
+import com.cseiu.passnet.sagaprofile.common.services.EventStoreService;
+import com.cseiu.passnet.sagaprofile.common.services.SagaStoreService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j(topic = "FailureResponseConsumer")
 public class FailureResponseConsumer implements IMessageConsumer<FailureResponse>{
+    private final SagaStoreService sagaStoreService;
+    private final EventStoreService eventStoreService;
+
+    @Autowired
+    public FailureResponseConsumer(SagaStoreService sagaStoreService, EventStoreService eventStoreService) {
+        this.sagaStoreService = sagaStoreService;
+        this.eventStoreService = eventStoreService;
+    }
+
     @Override
     public void consume(FailureResponse failureResponse) {
-
+        try {
+            this.sagaStoreService.removeSaga(failureResponse.getEventId());
+            log.info("removed saga");
+            log.info("compensate event [{}]", failureResponse.getEventId());
+//            var response = this.compensatingExecutorStub.rollback(ConsumeEvents.EventIdProtobuf.newBuilder().setEventId(failureResponse.getEventId()).build());
+        } catch (SagaNotFoundException exception) {
+            try {
+                log.info("saga not found");
+                this.eventStoreService.removeEvent(failureResponse.getEventId());
+                log.info("removed event");
+                log.info("compensate event [{}]", failureResponse.getEventId());
+//                var response = this.compensatingExecutorStub.rollback(ConsumeEvents.EventIdProtobuf.newBuilder().setEventId(failureResponse.getEventId()).build());
+            } catch (EventNotFoundException e) {
+                log.info("No Saga and Event found. This service did not consume event or already failed with event [{}]", failureResponse.getEventId());
+            }
+        }
     }
 }
